6b8c2fe117db7abae1486d4c83b4d153a4eab34e
mrsyafapri <rizkisyafapri@gmail.com>
2022-01-02 15:15:16 +0700

    pwa

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,20 @@ form.addEventListener('submit', e => {
 })
 // End:Contact Form
 
+// Start:PWA
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('sw.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+// End:PWA
+
 // Start:AOS
 AOS.init({
     once: true,
